76120864582e44e84777304b95463a89f0f648e2
Jana Bin Ail <jwalbinali@gmail.com>
2020-01-23 14:53:25 +0300

    done with  task

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+number = input("Enter a number from 1 to 12")
+items = input("Enter a noun (plural)")
+name = input("Enter a name").capitalize()
+scream = input ("Enter any sentence") .upper()
+action = input ("Enter a verb")
+
+print("It was %s o'clock when i heard a knock at the door" %(number))
+print('I opened the door and there was a box full of %s with a note saying "From %s"' %(items, name))
+print("Just as i closed the door I heard a %s" %(scream))
+print("I  froze in place and all I could do was %s" %(action))
+
+
+
+
+
+
+
+
+
